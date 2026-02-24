5196ff14c6c7ef6123702992f46701f5fd42605f
hee128 <eum.jin.hee8865@gmail.com>
2026-02-24 17:08:37 +0900

    엄진희 작업 1. 디테일 작업

--- js/best.js
+++ js/best.js
@@ -8,9 +8,27 @@ button.addEventListener('click',() => {
 }) ;
 
 
-const scrollBox = document.querySelector('.scroll_box');
 
-scrollBox.addEventListener('scroll', function() {
-    console.log('영역 스크롤 중');
-    console.log(scrollBox.scrollTop);
+//필터창 스크롤
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollBox = document.querySelector('.scroll_box');
+  if (scrollBox) {
+    scrollBox.addEventListener('scroll', function() {
+      console.log('영역 스크롤 중');
+      console.log(scrollBox.scrollTop);
+    });
+  }
+});
+
+
+//필터창 초기화
+document.addEventListener('DOMContentLoaded', () => {
+  const best_reset = document.querySelector('.best_reset');
+  const best_btns = document.querySelectorAll('.best_btn');
+
+  if (best_reset) {
+    best_reset.addEventListener('click', () => {
+      best_btns.forEach(btn => btn.classList.remove('active'));
+    });
+  }
 });
